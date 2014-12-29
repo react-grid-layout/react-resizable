1c2e285076c129160772536b2af2aa6d6c786dac
Samuel Reed <samuel.trace.reed@gmail.com>
2014-12-29 20:22:54 +0100

    Some refactoring

--- lib/Resizable.jsx
+++ lib/Resizable.jsx
@@ -27,38 +27,13 @@ var Resizable = module.exports = React.createClass({
   },
 
   minConstraints() {
-    return this.parseConstraints(this.props.minConstraints, this.props.handleSize[0]) || this.props.handleSize;
+    return parseConstraints(this.props.minConstraints, this.props.handleSize[0]) || this.props.handleSize;
   },
 
   maxConstraints() {
-    return this.parseConstraints(this.props.maxConstraints, this.props.handleSize[1]);
+    return parseConstraints(this.props.maxConstraints, this.props.handleSize[1]);
   },
 
-  /**
-   * Constraints must be subtracted by the size of the handle to work properly.
-   * This has a side-effect of effectively limiting the minimum size to the handleSize,
-   * which IMO is fine.
-   * @param  {Array} constraints Constraints array.
-   * @param  {Array} handleSize  Handle size array.
-   * @return {Array}             Transformed constraints.
-   */
-  parseConstraints(constraints, handleSize) {
-    if (!constraints) return;
-    return constraints.map(function(c) {
-      return c - handleSize;
-    });
-  },
-
-  /**
-   * Given left and top coords of the handle, figure out the width and height of the box.
-   * @param  {Number} options.left Left coord.
-   * @param  {Number} options.top  Top coord.
-   * @return {Object}              Width & height (px).
-   */
-  calcWH({left, top}) {
-    var s = this.props.handleSize;
-    return {width: left + s[0], height: top + s[1]};
-  },
 
   /**
    * Wrapper around drag events to provide more useful data.
@@ -69,20 +44,8 @@ var Resizable = module.exports = React.createClass({
   resizeHandler(handlerName) {
     var me = this;
     return function(e, {element, position}) {
-      this.props[handlerName] && this.props[handlerName](e, {element, size: this.calcWH(position)});
+      me.props[handlerName] && me.props[handlerName](e, {element, size: calcWH(position, me.props.handleSize)});
     };
-  },
-
-  onResizeStart(e, {element, position}) {
-    this.props.onResizeStart && this.props.onResizeStart(e, {element, size: this.calcWH(position)});
-  },
-
-  onResize(e, {element, position}) {
-    this.props.onResize && this.props.onResize(e, {element, size: this.calcWH(position)});
-  },
-
-  onResizeStop(e, {element, position}) {
-    this.props.onResizeStop && this.props.onResizeStop(e, {element, size: this.calcWH(position)});
   },
 
   render() {
@@ -93,15 +56,14 @@ var Resizable = module.exports = React.createClass({
     // A draggable handle.
 
     return React.addons.cloneWithProps(p.children, assign({}, p, {
-      // Array.isArray() check fixes react-hot-loader, doesn't seem to have other consequences
-      children: Array.isArray(p.children) ? p.children : [
+      children: [
         p.children.props.children,
         <Draggable
           start={{x: p.width - 20 + 'px', y: p.height - 20 + 'px'}}
           moveOnStartChange={true}
-          onStop={this.onResizeStop}
-          onStart={this.onResizeStart}
-          onDrag={this.onResize}
+          onStop={this.resizeHandler('onResizeStop')}
+          onStart={this.resizeHandler('onResizeStart')}
+          onDrag={this.resizeHandler('onResize')}
           minConstraints={this.minConstraints()}
           maxConstraints={this.maxConstraints()}
           >
@@ -111,3 +73,29 @@ var Resizable = module.exports = React.createClass({
     }));
   }
 });
+
+/**
+ * Parse left and top coordinates; we have to add the handle size to get the full picture.
+ * @param  {Number} options.left Left coordinate.
+ * @param  {Number} options.top  Top coordinate.
+ * @param  {Array}  handleSize   Handle data.
+ * @return {Object}              Coordinates
+ */
+function calcWH({left, top}, handleSize) {
+  return {width: left + handleSize[0], height: top + handleSize[1]};
+}
+
+/**
+ * Constraints must be subtracted by the size of the handle to work properly.
+ * This has a side-effect of effectively limiting the minimum size to the handleSize,
+ * which IMO is fine.
+ * @param  {Array} constraints Constraints array.
+ * @param  {Array} handleSize  Handle size array.
+ * @return {Array}             Transformed constraints.
+ */
+function parseConstraints(constraints, handleSize) {
+  if (!constraints) return;
+  return constraints.map(function(c) {
+    return c - handleSize;
+  });
+}
