5d519d952bdc523c0b8e4a8cd416263849691217
Samuel Reed <samuel.trace.reed@gmail.com>
2014-12-29 13:43:13 +0100

    Throw an error when instantiating directly

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = {
-  Resizable: require('./lib/Resizable.jsx'),
-  ResizableBox: require('./lib/ResizableBox.jsx')
+module.exports = function() {
+  throw new Error("Don't instantiate Resizable directly! Use require('react-resizable').Resizable");
 };
+
+module.exports.Resizable = require('./lib/Resizable.jsx');
+module.exports.ResizableBox = require('./lib/ResizableBox.jsx');
