bc7daca59e95bfeb3c0c86e4de7261b09c3bf408
Samuel Reed <samuel.trace.reed@gmail.com>
2016-02-10 11:36:03 -0600

    Add "browser" config to package.json and remove bundled react

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,21 @@ module.exports = {
         filename: "bundle.js",
         sourceMapFilename: "[file].map",
     },
+    externals: {
+      'react': {
+        'commonjs': 'react',
+        'commonjs2': 'react',
+        'amd': 'react',
+        // React dep should be available as window.React, not window.react
+        'root': 'React'
+      },
+      'react-dom': {
+        'commonjs': 'react-dom',
+        'commonjs2': 'react-dom',
+        'amd': 'react-dom',
+        'root': 'ReactDOM'
+      }
+    },
     module: {
       loaders: [
         {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader?cacheDirectory=true'},
