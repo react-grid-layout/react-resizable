5e7f9aacbc647060e2f8e69966ec1cbab8037db4
Samuel Reed <samuel.trace.reed@gmail.com>
2016-02-10 11:35:24 -0600

    Babel 6 export fix

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ module.exports = function() {
   throw new Error("Don't instantiate Resizable directly! Use require('react-resizable').Resizable");
 };
 
-module.exports.Resizable = require('./build/Resizable');
-module.exports.ResizableBox = require('./build/ResizableBox');
+module.exports.Resizable = require('./build/Resizable').default;
+module.exports.ResizableBox = require('./build/ResizableBox').default;
