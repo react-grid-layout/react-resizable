e8c40f376d9942e90d1580bf24a4d21320ecad62
Dylan Nissley <dylann@angieslist.com>
2016-09-05 13:10:33 -0400

    Modify build script to look for babel in the right place

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 rm -rf ./build
-./node_modules/.bin/babel --out-dir ./build ./lib
+if [ -d ./node_modules ]; then
+  ./node_modules/.bin/babel --out-dir ./build ./lib
+else
+  ../.bin/babel --out-dir ./build ./lib
+fi
 # More cross-platform compatible than `rename`
 find ./build -type f -name '*.jsx' -exec sh -c 'mv -f $0 ${0%.jsx}.js' {} \;
