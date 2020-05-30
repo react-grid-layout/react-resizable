6a064395f8eef180f1074e5fc4bb13af66843039
Daniele <daniele.cina@makeitapp.eu>
2020-05-30 11:21:41 +0200

    remove breaking change

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
   coverageThreshold: {
     global: {
-      branches: 15, // TODO: 80
-      functions: 15, // TODO: 80
-      lines: 15, // TODO: 80
-      statements: 15 // TODO: 80
+      branches: 30, // TODO: > 80
+      functions: 20, // TODO: > 80
+      lines: 60, // TODO: > 80
+      statements: 30 // TODO: > 80
     }
   },
   setupFiles: [
