8a6c162a25c7122fb06febb772d13702efbeb04a
STRML <STRML@users.noreply.github.com>
2025-12-31 04:54:28 +0000

    Deploying to gh-pages from @ react-grid-layout/react-resizable@deb5fe71d57c491204fcc64f9b2c1daa72401b06 🚀

--- test/test.js
+++
@@ -1,8 +0,0 @@
-import TestLayout from './TestLayout';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  var contentDiv = document.getElementById('content');
-  ReactDOM.render(React.createElement(TestLayout), contentDiv);
-});
